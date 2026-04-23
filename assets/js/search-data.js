040caac8344f748267b123a895ecfd0e00e13012
atomvn <hnvanhao@gmail.com>
2026-04-23 14:44:17 +0000

    Deploying to gh-pages from @ atomvn/haonguyen@1ffd1b6fb9fbf86de3ab36b9f9cde5da39ccd0c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,6 +41,17 @@ ninja.data = [{
         
           title: "...",
         
+        description: "this is what included plotly.js code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/haonguyen/blog/2026/plotly/";
+          
+        },
+      },{id: "post-",
+        
+          title: "...",
+        
         description: "this is what included image galleries could look like",
         section: "Posts",
         handler: () => {
