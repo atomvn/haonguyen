7d7189a37d1ec20803bb5ee7c3035857ca5b8de1
atomvn <hnvanhao@gmail.com>
2026-05-03 12:18:03 +0000

    Deploying to gh-pages from @ atomvn/haonguyen@b2ba81586db4f43fcab4f1bdf780b685ba6e1978 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -135,46 +135,11 @@ ninja.data = [{
           description: "UAV swarm system project is developed to solve problems in SAR missions after disasters such as: typhoon, hurricane, landslide...",
           section: "Projects",handler: () => {
               window.location.href = "/haonguyen/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/haonguyen/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/haonguyen/projects/3_project/";
             },},{id: "projects-fixed-wing-flight-controller",
           title: 'Fixed wing flight controller',
           description: "A simple flight controller utilizing STM32F103C8T6 microncontroller with some peripherals to control flight tasks such as: auto take off, normal, balancing flight mode...",
           section: "Projects",handler: () => {
               window.location.href = "/haonguyen/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/haonguyen/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/haonguyen/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/haonguyen/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/haonguyen/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/haonguyen/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
